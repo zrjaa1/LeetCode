f97d0683df767c57758d6840868eb87063c7d723
Jay <jaychang2016@outlook.com>
2019-03-03 17:50:01 -0800

    19.02.28 push from desktop

--- src/DP/JumpGameII.java
+++ src/DP/JumpGameII.java
@@ -30,6 +30,7 @@ Explanation: The minimum number of jumps to reach the last index is 2.
 Sol 1: DFS, 但时间过不了
 Sol 2: BFS, 时间还是过不了
 Sol 3: DP, 从右往左填
+Sol 4: Greedy
  */
 public class JumpGameII {
     // sol 2
@@ -57,6 +58,7 @@ public class JumpGameII {
     */
 
     // sol 3
+    /*
     public int jump(int[] nums) {
         if (nums == null || nums.length == 0) return -1;
         int n = nums.length;
@@ -72,6 +74,26 @@ public class JumpGameII {
             dp[i] = min+1;
         }
         return dp[0];
+    }
+    */
+
+    // sol 4
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int n = nums.length;
+        int maxRange = 0;
+        int cur = 0;
+        int step = 0;
+        while (maxRange < n-1) {
+            int newMax = 0;
+            for (int i = cur; i <= maxRange; i++) {
+                newMax = Math.max(newMax, i+nums[i]);
+            }
+            cur = maxRange+1;
+            maxRange = newMax;
+            step++;
+        }
+        return step;
     }
 
     public static void main() {
