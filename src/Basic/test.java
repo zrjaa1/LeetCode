8ca7a933b0b2d1d6a25befe7c514a4fce3273816
jay's iMac <531372539@qq.com>
2019-03-19 00:57:28 -0700

    Push from iMac 03.19.2019

--- src/Basic/test.java
+++ src/Basic/test.java
@@ -3,5 +3,14 @@ import java.util.*;
 
 // test for keyword - static
 public class test {
-
+    public static void main(String[] args) {
+        int[] tester = {1,3,5,7,9};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < tester.length; i++) {
+            map.put(tester[i], i);
+        }
+        int res = map.get(7);
+        System.out.println(res);
+        System.out.println(map.get(5));
+    }
 }
